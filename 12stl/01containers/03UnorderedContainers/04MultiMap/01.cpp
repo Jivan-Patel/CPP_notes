9ee13898917b576e1dc20c2af13780b9334566a9
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-25 21:33:14 +0530

    Unordered multi-map completed whit this STL contaier completed

---
+++ 12stl/01containers/03UnorderedContainers/04MultiMap/01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+
+    unordered_multimap<int, string> m1;
+    m1.insert({1, "Jivan"});
+    m1.insert({2, "Anand"});
+    m1.insert({3, "Rishab"});
+    m1.insert({1, "Jivan2"});
+
+    for (auto& [key, value] : m1) {
+        cout << key << ": " << value << endl;
+    }
+
+    return 0;
+}
